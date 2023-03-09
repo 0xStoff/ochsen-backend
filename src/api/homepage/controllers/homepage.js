b280efff8bc1dd84e2d8c5d4fdab1826f669e192
= <christoph.tiger@hotmail.com>
2023-03-09 18:21:07 +0100

    read logo from backend

--- src/api/homepage/controllers/homepage.js
+++ src/api/homepage/controllers/homepage.js
@@ -8,17 +8,32 @@ const {createCoreController} = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::homepage.homepage', {
 
-    async find(ctx) {
-        return await strapi.entityService.findMany('api::homepage.homepage', {
+    async find() {
+        const entries = await strapi.entityService.findMany('api::homepage.homepage', {
             fields: ['title', 'claim', 'event', 'caption'],
             populate: {
                 logo_positiv: {
-                    fields: 'url'
+                    fields: ['url', 'alternativeText']
                 },
                 logo_negativ: {
-                    fields: 'url'
+                    fields: ['url', 'alternativeText']
                 }
             },
         });
+
+        const { logo_positiv, logo_negativ, ...filteredEntries } = entries;
+        return {
+            ...filteredEntries,
+            logo: {
+                url: {
+                    light: logo_negativ?.url ?? null,
+                    dark: logo_positiv?.url ?? null,
+                },
+                alternativeText: {
+                    light: logo_positiv?.alternativeText ?? logo_negativ?.alternativeText,
+                    dark: logo_negativ?.alternativeText ?? logo_positiv?.alternativeText,
+                },
+            }
+        }
     },
 });
